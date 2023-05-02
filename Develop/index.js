3947919dc96b27bcb18105cbac248f1ca3eff84f
PurpxPanda <116929120+PurpxPanda@users.noreply.github.com>
2023-05-02 17:43:56 -0500

    Completed TODOs

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,67 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const process = require('process');
+const path = require('path');
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questions = [
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Please provide a brief description of your project:",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Please provide installation instructions for your project:",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Please provide usage instructions for your project:",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "Please choose a license for your project:",
+      choices: ["MIT", "GNU GPLv3", "Apache License 2.0", "ISC"],
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email?",
+    },
+  ];
+  
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log("README.md file generated successfully!");
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
-
+function init() {
+    inquirer
+      .prompt(questions)
+      .then((data) => {
+        const markdown = generateMarkdown(data);`
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,88 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+ //a function that returns a license badge based on which license is passed in
+// If there is no license, returns an empty string
+function renderLicenseBadge(license) {
+  if (!license) {
+    return "";
+  }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+  switch (license.toLowerCase()) {
+    case "mit":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    case "apache":
+      return "[![License: Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    case "gnu":
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+    case "mozilla":
+      return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+    default:
+      return "";
+  }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+//a function that returns the license link
+// If there is no license, returns an empty string
+function renderLicenseLink(license) {
+  if (!license) {
+    return "";
+  }
 
-// TODO: Create a function to generate markdown for README
+  switch (license.toLowerCase()) {
+    case "mit":
+      return "https://opensource.org/licenses/MIT";
+    case "apache":
+      return "https://opensource.org/licenses/Apache-2.0";
+    case "gnu":
+      return "https://www.gnu.org/licenses/gpl-3.0";
+    case "mozilla":
+      return "https://opensource.org/licenses/MPL-2.0";
+    default:
+      return "";
+  }
+}
+
+//a function that returns the license section of README
+// If there is no license, returns an empty string
+function renderLicenseSection(license, projectTitle) {
+  if (!license) {
+    return "";
+  }
+
+  const licenseLink = renderLicenseLink(license);
+  const licenseBadge = renderLicenseBadge(license);
+
+  return `## License
+
+${projectTitle} is licensed under the [${license}](${licenseLink}) license. ${licenseBadge}`;
+}
+
+//a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const { title, description, installation, usage, license } = data;
+  const licenseSection = renderLicenseSection(license, title);
 
+  return `# ${title}
+
+${licenseBadge}
+
+## Description
+
+${description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+
+## Installation
+
+${installation}
+
+## Usage
+
+${usage}
+
+${licenseSection}
 `;
 }
 
